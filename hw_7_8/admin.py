a462eb95556019572968ffaaff61e833aba6fd30
Valerii <valklei1964@gmail.com>
2025-04-09 20:27:38 +0200

    new migrations

--- hw_7_8/admin.py
+++ hw_7_8/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from hw_7_8.models import Category, Task, SubTask
 
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'categories__name', 'deadline', 'status')
+    list_filter = ('title', 'categories__name', 'deadline', 'status')
+    list_per_page = 2
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'deadline')
+    list_filter = ('title', 'status','deadline')
+    list_per_page = 2
+
+
+
+# admin.site.register(Task)
+#admin.site.register(SubTask)
 admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(SubTask)
--- hw_7_8/models.py
+++ hw_7_8/models.py
@@ -1,51 +1,59 @@
 from django.db import models
 
 
-STATUSES = [
-        ('NEW', 'New'),
-        ('IN_PROGRESS', 'In progress'),
-        ('PENDING', 'Pending'),
-        ('BLOCKED', 'Blocked'),
-        ('DONE', 'Done'),
-]
+status_choices = [
+        ("New", "New"),
+        ("In Progress", "In Progress"),
+        ("Pending", "Pending"),
+        ("Blocked", "Blocked"),
+        ("Done", "Done")
+    ]
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = "task_manager_category"
+        verbose_name = "Category"
+        unique_together = ("name",)
 
     def __str__(self):
         return self.name
 
+
+
 class Task(models.Model):
-    title = models.CharField(max_length=50, unique_for_date='deadline')
+    title = models.CharField(max_length=50, unique_for_date="deadline")
     description = models.TextField()
-    category = models.ManyToManyField(Category)
-    status = models.CharField(
-        max_length=30,
-        default='NEW',
-        choices=STATUSES)
+    categories = models.ManyToManyField(Category)
+    status = models.CharField(max_length=100, choices=status_choices, default="New")
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = "task_manager_task"
+        verbose_name = "Task"
+        ordering = ("-created_at",)
+        unique_together = ("title",)
+
     def __str__(self):
         return self.title
-
-
-
-# Create your models here.
 
 
 class SubTask(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    status = models.CharField(
-        max_length=30,
-        default='NEW',
-        choices=STATUSES)
+    status = models.CharField(max_length=100, choices=status_choices)
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = "task_manager_subtask"
+        ordering= ("-created_at",)
+        verbose_name = "SubTask"
+        unique_together = ("title",)
+
     def __str__(self):
         return self.title
-
