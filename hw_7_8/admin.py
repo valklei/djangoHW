ba0bdfe65efd632b267ada6d0b26fe8838729286
Valerii <valklei1964@gmail.com>
2025-04-14 08:57:23 +0200

    Change admin for 11 dz

--- hw_7_8/admin.py
+++ hw_7_8/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from hw_7_8.models import Category, Task, SubTask
-
+from django.db.models import QuerySet
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'categories__name', 'deadline', 'status')
-    list_filter = ('title', 'categories__name', 'deadline', 'status')
+    list_display = ('title', 'short_title', 'status', 'deadline')
+    search_fields = ('title',)
+    list_filter = ('status', 'deadline')
     list_per_page = 2
+    inlines = [SubTaskInline]
+
+    def short_title(self, obj: Task) -> str:
+        return f"{obj.title[:10]}..."
 
 
 @admin.register(SubTask)
@@ -14,7 +19,22 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'status', 'deadline')
     list_filter = ('title', 'status','deadline')
     list_per_page = 2
+    search_fields = ('title',)
 
+    actions = ['update_status_to_done']
+
+    def update_status_to_done(self, request, objects: QuerySet) -> None:
+        for obj in objects:
+            obj.status = 'Done'
+
+            obj.save()
+
+    update_status_to_done.short_description = "Change status to Done"
+
+
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+    extra = 1
 
 
 # admin.site.register(Task)
